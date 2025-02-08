6f34ca76a82b7c0c5032901dc1f7bbe35c357152
Carson <futgasansoo@gmail.com>
2025-02-07 21:08:26 -0500

    Initial commit

---
+++ App.py
@@ -0,0 +1,38 @@
+from contextlib import nullcontext
+from tkinter import*
+
+def add():
+    if(input.get() != ""):
+        list.delete(0,END)
+        todo_list.append(input.get())
+        input.delete(0,END)
+
+        for item in todo_list:
+            list.insert(END,item)
+
+def delete():
+    if(list.curselection()):
+        list.delete(list.curselection())
+
+window = Tk()
+window.resizable(FALSE,FALSE)
+
+window.bind("<Return>", lambda event: add_btn.invoke())
+window.bind("<Delete>", lambda event: complete_btn.invoke())
+
+add_btn = Button(window,text="Add Task",command=add)
+add_btn.grid(row=0,column=0)
+
+complete_btn = Button(window,text="Complete",command=delete)
+complete_btn.grid(row=1,column=0,sticky=N)
+
+input = Entry(window, font=("Arial",15))
+input.grid(row=0,column=1,sticky=E)
+
+todo_list = []
+
+list = Listbox(window)
+list.grid(row=1,column=1,sticky=E)
+list.config(font=("Arial",15))
+
+window.mainloop()
